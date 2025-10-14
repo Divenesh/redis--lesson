70760c1f3e4ca800cbd1d3f44627767a8a687677
Divenesh Office <divenesh.shamugam@gmail.com>
2025-10-14 09:25:43 +0800

    feat: added Redis caching to countries endpoint

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import { createClient } from 'redis';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const redisClient = createClient();
+
+redisClient.on('error', (err) => {
+  console.error('Redis Client Error', err);
+});
+
+await redisClient.connect();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -10,8 +18,19 @@ app.get('/', (req, res) => {
 
 
 app.get('/countries', async (req, res) => {
+  const cacheKey = 'countries';
+  const cachedData = await redisClient.get(cacheKey);
+
+  if (cachedData) {
+    console.log('Serving from cache');
+    return res.json(JSON.parse(cachedData));
+  }
   const countries = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
   const data = await countries.json();
+  console.log(data.length);
+  await redisClient.set(cacheKey, JSON.stringify(data), {
+    EX: 30,
+  });
   res.json(data);
 });
 
